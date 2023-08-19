09e97357f05793e519ff79b707652415d69c2962
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2023-08-19 20:06:36 +0700

    membuat dan menggunakan store

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <base-container title="Vuex">
+    <h1>{{ counter }}</h1>
+    <!-- <Test /> -->
+    <button @click="addOne">Add 1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter
+    }
+  },
+  methods: {
+    addOne() {
+      return this.$store.state.counter++
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
